0592d7ff1db839f2da2c6d7d098b250d79d69ed6
TedBaggins <tedbagginsrb@gmail.com>
2020-12-27 22:21:28 +0300

    base project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header section-shadow">
+            <img src="https://assets.stickpng.com/thumbs/5b447f12e99939b4572e32c0.png"></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+        <div className="content section-shadow">
+            <div>
+                <img src="https://pcvector.net/uploads/posts/2020-05/1588792159_blurred-light-background-min.jpg"></img>
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
