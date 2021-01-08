de8b98028858d96f8848361c7220506c229d6603
TedBaggins <tedbagginsrb@gmail.com>
2021-01-08 10:06:28 +0300

    class component users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,54 +3,48 @@ import styles from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../images/user-photo-default.jpg';
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
-
+    constructor(props) {
+        super(props);
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         });
-
-        // props.setUsers([
-        //     {id: 1, photoUrl: 'https://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg', followed: false, fullName: 'John', status: 'status', location: {city: 'city', country: 'country'}},
-        //     {id: 2, photoUrl: 'https://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg', followed: false, fullName: 'William', status: 'status', location: {city: 'city', country: 'country'}},
-        //     {id: 3, photoUrl: 'https://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg', followed: true, fullName: 'Mia', status: 'status', location: {city: 'city', country: 'country'}},
-        //     {id: 4, photoUrl: 'https://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg', followed: false, fullName: 'Samuel', status: 'status', location: {city: 'city', country: 'country'}},
-        //     {id: 5, photoUrl: 'https://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg', followed: true, fullName: 'Chloe', status: 'status', location: {city: 'city', country: 'country'}},
-        //     {id: 5, photoUrl: 'https://www.baytekent.com/wp-content/uploads/2016/12/facebook-default-no-profile-pic1.jpg', followed: false, fullName: 'James', status: 'status', location: {city: 'city', country: 'country'}}
-        // ]);
     }
 
-    return (
-        <div>
-            {
-                props.users.map( u => {
-                    return <div key={u.id}>
-                        <span>
-                            <div>
-                                <img className={styles.userPhoto} src={ u.photos.small != null ? u.photos.small : userPhoto}></img>
-                            </div>
-                            <div>
-                                { u.followed 
-                                    ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> 
-                                    : <button onClick={() => props.follow(u.id)}>Follow</button> }
-                            </div>
-                        </span>
-                        <span>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map( u => {
+                        return <div key={u.id}>
                             <span>
-                                <div>{u.name}</div>
-                                <div>{u.status}</div>
+                                <div>
+                                    <img className={styles.userPhoto} src={ u.photos.small != null ? u.photos.small : userPhoto}></img>
+                                </div>
+                                <div>
+                                    { u.followed 
+                                        ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button> 
+                                        : <button onClick={() => this.props.follow(u.id)}>Follow</button> }
+                                </div>
                             </span>
                             <span>
-                                <div>{"u.location.country"}</div>
-                                <div>{"u.location.city"}</div>
+                                <span>
+                                    <div>{u.name}</div>
+                                    <div>{u.status}</div>
+                                </span>
+                                <span>
+                                    <div>{"u.location.country"}</div>
+                                    <div>{"u.location.city"}</div>
+                                </span>
                             </span>
-                        </span>
-                    </div>
-                })
-            }
-        </div>
-    )
+                        </div>
+                    })
+                }
+            </div>
+        )
+    }
+    
 }
 
 export default Users;
