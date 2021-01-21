fa0b8be01d48a6b2ebafa1c9aed8973f26cd0e22
TedBaggins <tedbagginsrb@gmail.com>
2021-01-21 09:34:09 +0300

    react-test-renderer, profileStatus test

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import UsersContainer from './components/Users/UsersContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import {Route, withRouter} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import store from "./redux/redux-store";
 
 class App extends React.Component {
 
@@ -53,7 +54,24 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 });
 
-export default compose(
+// export default compose(
+//     withRouter,
+//     connect(mapStateToProps, {initializeApp})
+// )(App);
+
+let AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp})
 )(App);
+
+const SocialNetworkApp = (props) => {
+    return(
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppContainer/>
+            </Provider>
+        </BrowserRouter>
+    )
+}
+
+export default SocialNetworkApp;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/redux-store';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from "react-router-dom";
+import SocialNetworkApp from "./App";
 
     ReactDOM.render(
         <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                        <App/>
-                </Provider>
-            </BrowserRouter>
+            <SocialNetworkApp/>
         </React.StrictMode>,
         document.getElementById('root')
     );
